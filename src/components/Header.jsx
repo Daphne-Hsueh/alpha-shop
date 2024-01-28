9a7e5436a8d738469a6270320a11d60864596b79
Daphne-Hsueh <daphnewk23@gmail.com>
2024-01-28 12:28:19 +0800

    complete header

---
+++ src/components/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "../styles/Header.module.css"
+
+function Navitem({navText}) {
+  return (
+    <li className={styles.navItem}>
+      <a className={styles.navLink} href="#">
+        {navText}
+      </a>
+    </li>
+  )
+}
+function Navlogo({id,path}) {
+  return (
+    <li id={id} className={styles.navItem}>
+      <object
+        data={path}
+        className= {styles.navIcon} 
+        type="image/svg+xml"
+      ></object>
+    </li>
+  )
+}
+
+
+function Header() {
+  return (
+    <header className={styles.siteHeader}>
+      <div className={`${styles.headerContainer} mx-auto`}>
+        {/* navbar-toggle */}
+        <input id="navbar-toggle" className={styles.navbarToggle} type="checkbox" />
+        <label htmlFor="navbar-toggle" className={styles.burgerContainer}>
+          <object data="./public/icons/toggle.svg" className={`${styles.iconToggle} cursor-point`} />
+        </label>
+        {/* navbar-menu */}
+        <nav className={styles.navbarMenu}>
+          <ul className={`${styles.navList} ${styles.siteMenuList} mr-auto`}>
+            <Navitem navText="男款" />
+            <Navitem navText="女款" />
+            <Navitem navText="最新消息" />
+            <Navitem navText="客製商品" />
+            <Navitem navText="聯絡我們" />
+          </ul>
+          <ul className={`${styles.navList} ${styles.siteActionList}`}>
+            {/* search */}
+            <Navlogo path="./public/icons/search.svg" />
+            {/* cart */}
+            <Navlogo path="./public/icons/cart.svg" />
+            {/* theme toggle */}
+            <Navlogo id="theme-toggle" path="./public/icons/moon.svg" />
+            <Navlogo id="theme-toggle" path="./public/icons/sun.svg" />
+          </ul>
+        </nav>
+        {/* logo */}
+        <a className={styles.headerLogoContainer} href="#">
+          <img src="./public/icons/logo.svg"  />
+        </a>
+      </div>
+    </header>
+  );
+}
+export default Header;
